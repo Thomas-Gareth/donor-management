f414f2eb6a59396d4f4d1a40ce9e7a81c118dd92
Gareth Thomas <thomasgareth641@gmail.com>
2025-02-15 12:10:13 +0000

    Added  postinstall property

--- donor-management/vite.config.js
+++ donor-management/vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist', // Ensure Vercel picks up the correct output folder
-  },
-})
+  root: ".",  // Ensure it points to the project root
+  plugins: [react()]
+});
